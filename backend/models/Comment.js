92a3eef4d4055702afe43f2e1266921377bbc9c3
Richa Sawatkar <127492462+Richasawatkar@users.noreply.github.com>
2024-07-10 13:43:05 +0530

    Update Comment.js

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -7,7 +7,6 @@ const CommentSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   outfitId: { type: Schema.Types.ObjectId, ref: 'Outfit', required: true },
   text: { type: String, required: true },
-  date: { type: Date, default: Date.now }
 });
 
 export default mongoose.model('Comment', CommentSchema);
