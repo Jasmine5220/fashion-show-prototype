2a97627ee1363469b8ef9f08e4dd23b21b7e90d8
Jasmine Jayaswal <63915531+Jasmine5220@users.noreply.github.com>
2024-07-13 17:15:03 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-
-export const fetchItems = () => API.get('/items');
-export const fetchOutfits = () => API.get('/outfits');
